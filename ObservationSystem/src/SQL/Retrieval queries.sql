abdb84ce589f089a8f0d6c5a120672487efc6687
jwholme2 <jwholme2@ncsu.edu>
2013-10-10 19:55:17 -0400

    Update Retrieval queries.sql
    Updated comments

--- ObservationSystem/src/SQL/Retrieval queries.sql
+++ ObservationSystem/src/SQL/Retrieval queries.sql
@@ -1,4 +1,6 @@
---query 1
+--Retrieval Queries
+
+--QUERY 1. Find patients with the lowest weight amongst HIV patients.
 SELECT P.fname, P.lname 
 FROM WEIGHT W, PATIENT P
 WHERE W.PATIENTID=P.PATIENTID AND W.QTY IN
@@ -6,7 +8,8 @@ WHERE W.PATIENTID=P.PATIENTID AND W.QTY IN
 SELECT MIN(QTY)
 FROM WEIGHT, PROBLEMS
 WHERE WEIGHT.PATIENTID=PROBLEMS.PATIENTID and PROBLEMS.PNAME='HIV');
---query 2. Of all Obesity and High Risk Patients, find patients with the highest blood pressure.
+
+--QUERY 2. Of all Obesity and High Risk Patients, find patients with the highest blood pressure.
 SELECT distinct pat.patientid,pat.fname,pat.lname,bp.systolic
 FROM Patient pat
 INNER JOIN problems pro
@@ -23,7 +26,10 @@ AND bp.systolic  =
     /* this subquery finds the highest systolic pressure for the obesity/high risk patients*/
   )
   
---query 4. Find patients who live in same city as healthfriend.
+--QUERY 3. Find patients who have healthfriends with no outstanding alerts. 
+--TBD
+
+--QUERY 4. Find patients who live in same city as healthfriend.
 SELECT pat1.patientid,pat1.fname,pat2.patientid,pat2.fname,pat1.city,pat2.city,hf.dttm
 FROM Patient pat1
 INNER JOIN healthfriend hf
@@ -31,16 +37,18 @@ ON hf.patientid = pat1.patientid
 INNER JOIN Patient pat2
 ON pat2.patientid  = hf.healthfriendid where pat1.city = pat2.city
 
---query 5 /*LIST HEALTHFRIENDS FOR PATIENTX*/
+--QUERY 5. For PatientX, list their healthfriends, ordered by date in which friendships were initiated.
 SELECT HealthFriend.HealthFriendID
 FROM HEALTHFRIEND
 WHERE HEALTHFRIEND.PATIENTID=6
 ORDER BY DTTM DESC
 
---reporting query 1. For each patient, find the number of healthfriends made in the last month.
+--REPORTING QUERY
+
+--QUERY 1. For each patient, find the number of healthfriends made in the last month.
 SELECT * FROM healthfriend hf WHERE hf.dttm >=  sysdate - INTERVAL '1' month(1);
 
---2. For each patient and each type of observation, show the number of such observations recorded by the patients.
+--QUERY 2. For each patient and each type of observation, show the number of such observations recorded by the patients.
 SELECT pat.patientid,pat.fname,'bloodpressure' as "observation type",count(bpid)
 FROM Patient pat
 LEFT JOIN bloodpressure bp
@@ -103,3 +111,4 @@ group by pat.patientid,pat.fname
 
 
 --3. For each patient, and each of their healthfriends, list the number of lingering alerts of the healthfriend.
+--TBD
