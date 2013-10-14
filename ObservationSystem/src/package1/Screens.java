80356e9eb9c4663258ff4f31f07a3f50d219646e
mxumichael <mxu@ncsu.edu>
2013-10-14 19:38:29 -0400

    commiting screen additions
    also the sqltools class. it's pretty bare at the moment.

--- ObservationSystem/src/package1/Screens.java
+++ ObservationSystem/src/package1/Screens.java
@@ -3,11 +3,13 @@ package package1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.SQLException;
 
 public class Screens {
 	static int LOOP_LIMIT = 5;
 	static String LOOP_LIMIT_ERROR= "Looplimitout";
 	BufferedReader in;
+	double patientId= 0;
 
 	public static void main(String args[]){
 
@@ -87,4 +89,134 @@ public class Screens {
 		System.out.println("reached Looplimit "+ LOOP_LIMIT + " in create user screen, going to previous screen");
 		return LOOP_LIMIT_ERROR;
 	}
+	private String patientStartScreen() throws IOException{
+
+		String userChoice = "";
+
+		for (int x= 0; x< LOOP_LIMIT; x++){
+			System.out.println("  1. Enter Observations         ");
+			System.out.println("  2. View Observations          ");
+			System.out.println("  3. Add a New Observation Type ");
+			System.out.println("  4. View MyAlerts              ");
+			System.out.println("  5. Manage HealthFriends       ");
+			System.out.println("  6. Back                       ");
+			System.out.println("  Enter choice                  ");
+			userChoice = in.readLine();
+			if (userChoice.equals("1")){
+				this.EnterObservations();
+			} else if (userChoice.equals("2")){
+				this.ViewObservations();
+			} else if (userChoice.equals("3")){
+				this.NewObservationType();
+			} else if (userChoice.equals("4")){
+				this.ViewMyAlerts();
+			} else if (userChoice.equals("5")){
+				this.ManageHealthFriends();
+			} else if (userChoice.equals("6")){
+				return("6. Back");
+			}
+		}
+		System.out.println("reached Looplimit "+ LOOP_LIMIT + " in login screen, going to previous screen");
+		return LOOP_LIMIT_ERROR;
+		}
+
+	private void ManageHealthFriends() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void ViewMyAlerts() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void NewObservationType() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void ViewObservations() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private String EnterObservations() throws IOException {
+		// TODO Auto-generated method stub
+
+		String userChoice = "";
+
+		for (int x= 0; x< LOOP_LIMIT; x++){
+			System.out.println("  1. Diet      ");
+			System.out.println("  2. Weight    ");
+			System.out.println("  3. Exercise  ");
+			System.out.println("  4. Mood      ");
+			System.out.println("  5. Other     ");
+			System.out.println("  6. Back      ");
+			System.out.println("  Enter choice                  ");
+			userChoice = in.readLine();
+			if (userChoice.equals("1")){
+				this.DietObservation();
+			} else if (userChoice.equals("2")){
+				this.WeightObservation();
+			} else if (userChoice.equals("3")){
+				this.ExerciseObservation();
+			} else if (userChoice.equals("4")){
+				this.MoodObservation();
+			} else if (userChoice.equals("5")){
+				this.OtherObservation();
+			} else if (userChoice.equals("6")){
+				return("6. Back");
+			}
+		}
+		System.out.println("reached Looplimit "+ LOOP_LIMIT + " in login screen, going to previous screen");
+		return LOOP_LIMIT_ERROR;
+		}
+
+	private void OtherObservation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void MoodObservation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void ExerciseObservation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void WeightObservation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private String DietObservation() throws IOException {
+		// TODO Auto-generated method stub
+		String description = "";
+		String amount = "";
+		String dateTime = "";
+		for (int x= 0; x< LOOP_LIMIT; x++){
+			System.out.println("  Enter:                                                                ");
+			System.out.print("What food or drink was consumed");
+			description = in.readLine();
+			System.out.print("Amount in servings:");
+			amount = in.readLine();
+			System.out.print("Date time of observation (YYYYMMDD HH:MI):");
+			dateTime = in.readLine();
+			
+			try {
+				SqlTools.insertDietObservation(description,amount,dateTime,patientId);
+				return "Observation Inserted Successfully";
+			}catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("observation input not Valid, please try again.");
+			}
+		}
+		System.out.println("reached Looplimit "+ LOOP_LIMIT + " in login screen, going to previous screen");
+		return LOOP_LIMIT_ERROR;		
+	}
+	
 }
--- ObservationSystem/src/package1/SqlTools.java
+++ ObservationSystem/src/package1/SqlTools.java
@@ -1,10 +1,49 @@
 package package1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class SqlTools {
 
 	public static Boolean LoginValid(String username, String password) {
 		// TODO This subroutine takes a username and password and checks if it's valid.
 		return true; //uhhhh.... sure, that's a valid login i dunno :P
+	}
+
+	public static boolean insertDietObservation(String description, String amount,
+			String dateTime, double patientId) throws SQLException {
+		// TODO Auto-generated method stub
+		//URL of Oracle database server
+		String url = "jdbc:oracle:thin:@ora.csc.ncsu.edu:1521:ORCL"; 
+
+		//properties for creating connection to Oracle database
+		Properties props = new Properties();
+		props.setProperty("user", "mxu");
+		props.setProperty("password", "abc123");
+		
+		//creating connection to Oracle database using JDBC
+		Connection conn = DriverManager.getConnection(url,props);
+		try{
+		String sql ="insert into Diet(dietid,patientid,description,qty,dttm,rec_dttm) values (Diet_seq.nextval(),"
+				+ patientId + "," + description + "," + amount + ",to_date(" + dateTime + ",'YYYYMMDD HH:MI'),sysdate);";
+
+		//creating PreparedStatement object to execute query
+		PreparedStatement preStatement = conn.prepareStatement(sql);
+
+		//executing the query and then committing it before closing in the finally block.
+		boolean result = preStatement.execute();		
+		conn.commit();
+		
+		return result;
+		}
+		finally {
+			conn.close();
+		}
+		
 	}
 
 
